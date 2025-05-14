a01dd6b7b7d9b146f58fa1341b41bc9786448b41
Diego Costa <diegocosta.doc23@gmail.com>
2025-05-14 14:51:19 -0300

    incluindo exercícios 2 e 3

--- script.js
+++ script.js
@@ -9,4 +9,22 @@
     }
 }) */
 
+// Exercício 2
+
+/* let listaNumeros = [65, 44, 12, 4];
+
+listaNumeros.forEach(numero => console.log(numero * 10)); */
+
+// Exercício 3
+
+let quadrado = document.querySelector(".quadrado");
+
+quadrado.addEventListener("click", () => {
+
+    if (quadrado.classList.contains("azul")) {
+        quadrado.classList.remove("azul");
+    } else {
+        quadrado.classList.add("azul");
+    }
+});
 
