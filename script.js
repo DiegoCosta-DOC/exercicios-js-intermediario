1e850cd3cd3ad03a60f1b811fca2e7e96d5c9ebd
Diego Costa <diegocosta.doc23@gmail.com>
2025-05-14 13:34:36 -0300

    subindo primeiro exercício

---
+++ script.js
@@ -0,0 +1,12 @@
+
+    // Exercício 1
+/* document.querySelector("button").addEventListener("click", function() {
+    let elemento = document.getElementById("azul");
+    if (elemento.style.backgroundColor == "blue") {
+        elemento.style.backgroundColor = "yellow";
+    } else {
+        elemento.style.backgroundColor = "blue";
+    }
+}) */
+
+
